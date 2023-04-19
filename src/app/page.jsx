1150d2cc34c108db52020db90ee2744baee7ffc2
silva4dev <web4dev.lucas@outlook.com>
2023-04-19 00:00:29 -0300

    inicial

---
+++ src/app/page.jsx
@@ -0,0 +1,30 @@
+export const dynamic = "force-dynamic";
+
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const response = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await response.json();
+
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
+}
